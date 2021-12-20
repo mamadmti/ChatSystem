479bfdac7b47d630f84c03bce999ba4139885128
Mokhtari <Mokhtari@DESKTOP-1N8UB2J>
2021-12-20 17:17:37 +0330

    first commit

---
+++ SignalR/Hubs.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR
+{
+    public class ChatHub : Hub
+    {
+    
+
+        //public async Task JoinGroup (string GroupId)
+        //{
+        //    await Groups.AddToGroupAsync(Context.ConnectionId, GroupId);
+        //}
+        //public async Task SendMessageGroup (string user, string message,string GroupId)
+        //{
+            
+        //    await Clients.Group(GroupId).SendAsync("ReceiveMessage", user, message);
+        //}
+        //public async Task SendMessage(string user, string message)
+        //{
+
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+    }
+}
---
+++ SignalR/wwwroot/js/Chat.js
@@ -0,0 +1,1 @@
+﻿//here build a connection
